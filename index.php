7fdc04d699129118b1945375abeef1e4536b5907
root <root@tsw-M51Vr.(none)>
2014-05-03 22:48:12 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+        <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="Chart/Chart.js"></script>
+        <script type="text/javascript" src="js/functions.js"></script>
+    
+    
+    
+    
+    </head>
+
+    <body>
+        Prueba de test de PREDIS
+        
+        <div class="container">
+            
+            
+            
+            
+        </div>
+        
+        
+        
+        
+        
+    </body>
+
+
+
+</html>
+
+
+
+
+
+
+
+
+
+
